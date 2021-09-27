fa9c3be711baf6b63467effec032d982cf245453
sialvsic <sialvsic@outlook.com>
2021-09-27 11:10:56 +0800

    update build command

--- .umirc.ts
+++ .umirc.ts
@@ -3,6 +3,23 @@ import { defineConfig } from 'umi';
 let publicPath = '/';
 let base = '/';
 
+switch (process.env.APP_ENV) {
+  case 'test':
+    publicPath = '/dist/';
+    base = '/dist';
+    break;
+
+  case 'prod':
+    publicPath = '/dist/';
+    base = '/dist';
+    break;
+
+  default:
+    publicPath = '/';
+    base = '/';
+    break;
+}
+
 export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
